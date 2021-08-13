683cd3616959e313d8f12549f918a6e6e390f81d
matisfd <matisfd@gmail.com>
2021-08-13 11:14:58 -0300

    modificacion en index - llamadas a funciones

--- index.php
+++ index.php
@@ -4,74 +4,15 @@
   $MP = new SAPIDK_MP\Action(); 
 
 //   //Listar tiendas
-// 	$qrCaja=  $MP->StoreList(804766238, 30, 0, "Your_ACCES_TOKEN");
+// 	 $qrCaja=  $MP->StoreList(804766238, 30, 0, "Your_ACCES_TOKEN");
 //   var_dump($qrCaja);
 
 
-  //Crear tienda
-
-  // $data = array (
-
-	// 	'name' => 'Sucursal Instore',
-	// 	'external_id' => '1234',
-	// 	'business_hours' =>  array (
-	// 		'monday' => array (
-	// 			0 =>  array (
-	// 				'open' => '08:00',
-	// 				'close' => '12:00',
-	// 			),
-	// 			1 =>  array (
-	// 				'open' => '14:00',
-	// 				'close' => '18:00',
-	// 			),
-	// 		),
-
-	// 		'tuesday' => array (
-	// 		  	0 => array (
-	// 		        'open' => '09:00',
-	// 		        'close' => '18:00',
-	// 		    ),
-	// 		),
-	// 	),
-	// 	'location' =>  array (
-	// 		'street_number' => '3039',
-	// 		'street_name' => 'Caseros',
-	// 		'city_name' => 'Belgrano',
-	// 		'state_name' => 'Capital Federal',
-	// 		'latitude' => -32.8897322,
-	// 		'longitude' => -68.8443275,
-	// 		'reference' => '3er Piso',
-	// 	),
-
-	// );
-
-
-
-	// $tiendaNueva=$MP->StoreNew($data, ULTIMOS_9_DIGITOS_Your_ACCES_TOKEN, "Your_ACCES_TOKEN");
-  // var_dump($tiendaNueva);
-
-
-
-  //Crear Caja
-  // $data = array (
-
-	// 	'name' => "NombredeQR/Caja", 
-	// 	'fixed_amount' => false,  // si es false EL CLIENTE INGRESA EL MONTO, si es true el monto lo indicamos nosotros
-	// 	// 'store_id' => "idstore",  Identificador de la sucursal a la que pertenece el Punto de Venta, es omitible
-	// 	'external_id' => mt_rand(3,20000), // Identificador único de la caja definido por el integrador, le recomendamos usar mt_rand(3,20000) 
-
-	// );
-
-
-	// $MP->NewBox($data, "Your_ACCES_TOKEN");
-
-
-
-  // Suscripcion
+//   Crear suscripcion
 //   $data = array (
 
 //     'reason' => 'Nombre_suscripcion',
-//     'collector_id' => ULTIMOS_9_DIGITOS_Your_ACCES_TOKEN,
+//     'collector_id' => Ultimosdigitos_acces-token,
 //     'status' => 'active',
 //     'back_url' => 'https://www.google.com.ar/',
 //     'external_reference' => 1212, // referencia interna
@@ -87,19 +28,16 @@
 
 // );
 
-
-
 // $datos = $MP->RecurrentCreator($data, "Your_ACCES_TOKEN");
-
 // var_dump($datos);
-// var_dump($MP->IPN("ULTIMOS_9_DIGITOS_ACCES_TOKEN", "Your_ACCES_TOKEN")); 
-
-
-// $movimientos = $MP->PaymentList(30, 0, "Your_ACCES_TOKEN");
-// var_dump($movimientos);
 
 
 
+//Listar IPN
+// var_dump($MP->IPN("Ultimosdigitos_acces-token", "Your_ACCES_TOKEN")); 
+
+
+//Listar movimientos de una cuenta
 // $movimientos=$MP->PaymentList(30, 0, "Your_ACCES_TOKEN");
 // var_dump($movimientos);
 
